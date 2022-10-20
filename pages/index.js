c54e0bc4ca51e6e19220fceec1fd747eb9fb2741
kimchi <666516lkc@gmail>
2022-10-20 14:18:43 +0800

    initial

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+// import ManualHeader from '../components/ManualHeader'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import LotteryEntrance from '../components/LotteryEntrance'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our Lottery Contract" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <ManualHeader></ManualHeader> */}
+      <Header></Header>
+      <LotteryEntrance></LotteryEntrance>
+    </div>
+  )
+}
